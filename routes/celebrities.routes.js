4b95705fa536b248e63b1497ad8b302ac1084e1a
Cecilia Torreira <celia.umbreon@gmail.com>
2023-05-09 15:48:34 +0100

    Iteración 3 completada

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,15 +1,39 @@
+const Celebrity = require("../models/Celebrity.model");
+
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
 
 // all your routes here
 
 
+//* GET "/celebrities/create" => Renderiza la vista con el formulario para añadir nuevas celebrities
+
+router.get("/create", (req, res, next)=>{
+
+  res.render("celebrities/new-celebrity.hbs")
 
 
+})
+
+//* POST "/celebrities/create" => Recibe información del usuario para crear una nueva celebridad y añadirla a la base de datos
+
+router.post("/create", (req, res, next)=>{
+  Celebrity.create({
+    name: req.body.name,
+    occupation: req.body.occupation,
+    catchPhrase: req.body.catchPhrase
+
+  })
+  .then(()=>{
+   console.log("Nueva celebridad añadida")
+   //res.redirect("/celebrities/list")  // Vista no creada tosavía
+   })
+  .catch((error)=>{
+    next(error)
+  })
 
 
-
-
+})
 
 
 
