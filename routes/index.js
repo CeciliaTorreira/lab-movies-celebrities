61c97dde55b57fb3bee31c905e95b3d8ac471430
Cecilia Torreira <celia.umbreon@gmail.com>
2023-05-09 15:21:26 +0100

    Iteración 1 hecha

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,17 @@
 const router = require("express").Router();
 
+
+//* REQUIRE AMBOS ARCHIVOS DE RUTAS DE CELEBRITIES Y MOVIES
+
+
+const celebritiesRouter = require("./celebrities.routes.js")
+router.use("/celebrities", celebritiesRouter)
+
+
+const moviesRouter = require("./movies.routes.js")
+router.use("/movies", moviesRouter)
+
+
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
